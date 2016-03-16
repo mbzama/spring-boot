76666f969d65033c99aa3ecc61b4b289d0cce5af
Mohammad Badiyu Zama <zamamb@gmail.com>
2016-03-16 16:18:04 +0530

    Added sample controller and testcases

--- src/main/java/zama/learning/Application.java
+++ src/main/java/zama/learning/Application.java
@@ -1,13 +1,24 @@
 package zama.learning;
 
+import java.util.Arrays;
+
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
-
+	private static Logger LOGGER = Logger.getLogger(Application.class);
 	public static void main(String[] args) {
-		System.out.println("hello");
-		SpringApplication.run(Application.class, args); 
+		ApplicationContext ctx = SpringApplication.run(Application.class, args);
+		LOGGER.info("Spring boot loading beans ... ");
+		
+		String[] beanNames = ctx.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		
+		for (String beanName : beanNames) {
+			LOGGER.info(beanName);
+		}
 	}
 }
---
+++ src/main/java/zama/learning/controller/HelloController.java
@@ -0,0 +1,13 @@
+package zama.learning.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String index(){
+		return "Spring boot demo!!!";
+	}
+}
